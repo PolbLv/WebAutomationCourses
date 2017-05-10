0a0f1f50deb0d423fc53fd027a8cd7b7e2d904b0
LVIVSOFT\spolyakov <s.polb.lv@gmail.com>
2017-05-10 15:03:25 +0300

    Update

--- src/test/java/com/valentine/test/TCOneVerifyIfButtonAddToCartWorks.java
+++ src/test/java/com/valentine/test/TCOneVerifyIfButtonAddToCartWorks.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TC1_1_VerifyIfButtonAddToCartWorks {
+public class TCOneVerifyIfButtonAddToCartWorks {
     @Test
     public static void testAddButtonWorks() {
 
--- src/test/java/com/valentine/test/VerifyIfyButtonMoreInfoWorks.java
+++
@@ -1,7 +0,0 @@
-package com.valentine.test;
-
-/**
- * Created by LVIVSOFT\spolyakov on 10.05.17.
- */
-public class VerifyIfyButtonMoreInfoWorks {
-}
